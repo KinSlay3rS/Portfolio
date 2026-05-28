b1467e671f2c1f976f2353204a1554ac823dffdc
Rahul_Singh <rhlsngh1025@gmail.com>
2026-05-28 12:39:49 +0530

    testing env

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 document.getElementById('greeting').textContent = "Deployed successfully at 28th May, 2026!";
+const apiKey = process.env.API_KEY;
+const isFeatureOn = process.env.FEATURE_FLAG === 'true';
